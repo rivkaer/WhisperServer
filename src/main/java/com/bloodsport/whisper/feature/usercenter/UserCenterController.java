6d80b9f7793778464d73ada3abbae73e1dac6886
lumo <rivkaer@qq.com>
2020-02-19 14:27:04 +0800

    :art: 更新加解密的单元测试流程

--- src/main/java/com/bloodsport/whisper/feature/usercenter/UserCenterController.java
+++ src/main/java/com/bloodsport/whisper/feature/usercenter/UserCenterController.java
@@ -5,7 +5,10 @@ import com.bloodsport.whisper.example.KeyValueDatabase;
 import com.bloodsport.whisper.interf.TokenHelper;
 import com.bloodsport.whisper.model.pojo.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
